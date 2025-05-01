4a0bab95b25af7e776cfc95bd61df78cc69228d8
Anastasiya1301 <dudkoanastasiya1301@gmail.com>
2025-05-01 20:31:32 +0300

    початок

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// 'use strict'
+let age = 25; //дає змінити значення, console-ні, number
+age = 35;
+
+console.log("Значення змінної age:", age); //дозволяє подивитись значення змінної в консолі
+
+const name = "Mike";//string
+const totalPrice = 9.50;
+const isLogged = true; //boolean
+const isOpen = false; //boolean
+console.log(age, name, totalPrice);
+let result; //indefined
+console.log(result);
+let newResult = null;
+console.log(newResult);
+const lastName = "Petrenko";
+console.log(lastName)
