ca685f0ea5dce9af0f12a74cb6674c3073cbaf85
Anastasiya1301 <dudkoanastasiya1301@gmail.com>
2025-05-03 19:56:53 +0300

    js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-const name = "Ted";
+// const name = "Ted";
--- js/index.js
+++ js/index.js
@@ -1,17 +1,62 @@
 // 'use strict'
-let age = 25; //дає змінити значення, console-ні, number
-age = 35;
+// let age = 25; //дає змінити значення, console-ні, number
+// age = 35;
 
-console.log("Значення змінної age:", age); //дозволяє подивитись значення змінної в консолі
+// console.log("Значення змінної age:", age); //дозволяє подивитись значення змінної в консолі
 
-const name = "Mike";//string
-const totalPrice = 9.50;
-const isLogged = true; //boolean
-const isOpen = false; //boolean
-console.log(age, name, totalPrice);
-let result; //indefined
-console.log(result);
-let newResult = null;
-console.log(newResult);
-const lastName = "Petrenko";
-console.log(lastName)
+// const name = "Mike";//string
+// const totalPrice = 9.50;
+// const isLogged = true; //boolean
+// const isOpen = false; //boolean
+// console.log(age, name, totalPrice);
+// let result; //indefined
+// console.log(result);
+// let newResult = null;
+// console.log(newResult);
+// const lastName = "Petrenko";
+// console.log(lastName);
+
+
+// const name = 'Nastya';
+// console.log(name);
+
+// const surname = 'Dudko';
+
+// const fullname = name + '' + surname;
+
+// console.log(fullname);
+
+// const num1 = '10';
+
+// const num2 = 20;
+
+// let result = num1 + num2;
+// console.log(num1)
+// console.log( typeof result);
+
+// const isLogged = true;
+// console.log(typeof isLogged);
+
+// const API_KEY = '123456';
+// console.log(API_KEY);
+
+// const warningMessage = 'Для доступу на сайт потрібна попередня реєстрація';
+// // window.alert(warningMessage);
+// alert(warningMessage);
+
+// const country = 'Ukraine';
+// console.log('Значення змінної country:', country);
+
+
+// const message = 'Ви підтверджуєте ваше замовлення?';
+// const isConfirmedOrder = confirm(message);
+// console.log(isConfirmedOrder);
+
+const newMessage = 'Введіть число';
+prompt(newMessage);
+console.log(prompt(newMessage));
+console.log(typeof prompt(newMessage));
+
+
+//Types 
+//number,string,blight,symbol,null,undefinded,boolean.
